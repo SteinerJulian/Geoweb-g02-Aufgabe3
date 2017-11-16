9e43131a616d2cf7e5d2c41a812b553d4fad81bd
chobenaus <chobenaus11@gmail.com>
2017-11-16 17:23:07 +0100

    mape v_5

--- main.js
+++ main.js
@@ -19,6 +19,11 @@ import {
 
 import 'ol/ol.css';
 
+var map = apply(
+  "map",
+  "https://gist.githubusercontent.com/anonymous/efb8d2ab014796b45817954ed4e188ba/raw/4e15466382a261a6e7da9cecfc34c5dc05f28525/style.json"
+);
+
 //apply('map', 'style.json');
 
 /*var xhr = new XMLHttpRequest();
