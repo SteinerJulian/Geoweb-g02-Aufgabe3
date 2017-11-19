a7efdf00877c4469e739b401a5d1f3ac7175f321
Bananathan <33088681+Bananathan@users.noreply.github.com>
2017-11-19 22:07:36 +0100

    main.js

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
 import 'ol/ol.css';
 import 'javascript-autocomplete/auto-complete.css';
+import Map from 'ol/map';
+import View from 'ol/view';
+import TileLayer from 'ol/layer/tile';
+import XYZSource from 'ol/source/xyz';
+//! [import-proj]
 import proj from 'ol/proj';
-import GeoJSON from 'ol/format/geojson';
+//! [import-proj]
 import VectorLayer from 'ol/layer/vector';
 import VectorSource from 'ol/source/vector';
-import {apply} from 'ol-mapbox-style';
+import Feature from 'ol/feature';
+import Point from 'ol/geom/point';
+import Style from 'ol/style/style';
+import IconStyle from 'ol/style/icon';
+import Overlay from 'ol/overlay';
+//! Suchzeile
+import GeoJSON from 'ol/format/geojson';
+import {
+  apply
+} from 'ol-mapbox-style';
 import AutoComplete from 'javascript-autocomplete';
 
-var map = apply(
-  'map',
-  'https://gist.githubusercontent.com/anonymous/efb8d2ab014796b45817954ed4e188ba/raw/4e15466382a261a6e7da9cecfc34c5dc05f28525/style.json'
-);
+//! [map-const]
+const map = new Map({
+  //! [map-const]
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new XYZSource({
+        url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg'
+      })
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 19
+  })
+});
 
 function fit() {
   map.getView().fit(source.getExtent(), {
@@ -19,52 +45,72 @@ function fit() {
   });
 }
 
-var selected;
-function getAddress(feature) {
-  var properties = feature.getProperties();
-  return (
-    (properties.city || properties.name || '') +
-    ' ' +
-    (properties.street || '') +
-    ' ' +
-    (properties.housenumber || '')
-  );
-}
+const position = new VectorSource();
+const vector = new VectorLayer({
+  source: position
+});
 
 var searchResult = new VectorLayer({
-  zIndex: 9999
+  zIndex: 1
 });
 map.addLayer(searchResult);
 
-var onload, source;
 new AutoComplete({
   selector: 'input[name="q"]',
   source: function(term, response) {
-    if (onload) {
-      source.un('change', onload);
-    }
-    searchResult.setSource(null);
-    source = new VectorSource({
+    var source = new VectorSource({
       format: new GeoJSON(),
       url: 'https://photon.komoot.de/api/?q=' + term
     });
-    onload = function(e) {
+    source.on('change', function() {
       var texts = source.getFeatures().map(function(feature) {
-        return getAddress(feature);
+        var properties = feature.getProperties();
+        return (properties.city || properties.name || '') + ', ' +
+          (properties.street || '') + ' ' +
+          (properties.housenumber || '');
       });
       response(texts);
-      fit();
-    };
-    source.once('change', onload);
-    searchResult.setSource(source);
-  },
-  onSelect: function(e, term, item) {
-    selected = item.getAttribute('data-val');
-    source.getFeatures().forEach(function(feature) {
-      if (getAddress(feature) !== selected) {
-        source.removeFeature(feature);
-      }
+      map.getView().fit(source.getExtent(), {
+        maxZoom: 19,
+        duration: 250
+      });
     });
-    fit();
+    searchResult.setSource(source);
+  }
+});
+
+vector.setStyle(new Style({
+  image: new IconStyle({
+    src: './data/marker.png'
+  })
+}));
+map.addLayer(vector);
+
+//! [geolocation]
+navigator.geolocation.getCurrentPosition(function(pos) {
+  const coords = proj.fromLonLat([pos.coords.longitude, pos.coords.latitude]);
+  map.getView().animate({
+    center: coords,
+    zoom: 13
+  });
+  position.addFeature(new Feature(new Point(coords)));
+});
+
+//! Pop-up bei Klick
+
+var overlay = new Overlay({
+  element: document.getElementById('popup-container'),
+  positioning: 'bottom-center',
+  offset: [0, -10]
+});
+
+map.addOverlay(overlay);
+
+map.on('click', function(e) {
+  overlay.setPosition();
+  var features = map.getFeaturesAtPixel(e.pixel);
+  if (features) {
+    overlay.getElement().innerHTML = 'Meine Position';
+    overlay.setPosition(e.coordinate);
   }
 });
